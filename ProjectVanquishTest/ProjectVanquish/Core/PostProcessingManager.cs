425c2609b757408d0d6579d0706896df70f17a55
SND\Neil_Knight_cp <SND\Neil_Knight_cp@527b9aa4-23f3-4c7d-acba-bb8337ca51bc>
2012-07-17 20:09:29 +0000

    git-svn-id: https://projectvanquish.svn.codeplex.com/svn@20468 527b9aa4-23f3-4c7d-acba-bb8337ca51bc

---
+++ ProjectVanquishTest/ProjectVanquish/Core/PostProcessingManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ProjectVanquish.Cameras;
+
+namespace ProjectVanquish.Core
+{
+    public class PostProcessingManager
+    {
+        public PostProcessingManager(Game game)
+        {
+        }
+
+        public void Draw(RenderTarget2D sceneRT, Camera camera)
+        {
+        }
+    }
+}
